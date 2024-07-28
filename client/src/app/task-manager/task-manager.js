deb1b0e506916ff8d626a0bdf67fd777fa2ad40f
Thili02 <agilesoftwareprojectx@gmail.com>
2024-07-28 19:07:44 +0800

    Made minor changes to task form and task list

--- client/src/app/task-manager/task-manager.js
+++ client/src/app/task-manager/task-manager.js
@@ -7,14 +7,21 @@ import { Navbar } from "react-bootstrap";
 export default function taskManager() {
   return(
     <>
-      <NavBar />
-      <div className="right-section">
-          <div className="transaction-form">
+      {/* <NavBar /> */}
+      <div className="left-section">
+          <div className="task-form">
             <form>
               <input type="text" placeholder="Title" />
               <input type="date" placeholder="Current date" />
-              <input type="text" placeholder="Category" />
-              <input type="number" placeholder="$" />
+              <select className="category">
+                <option value="default">Choose a category</option>
+                <option value="Type1">Type1</option>
+                <option value="Type2">Type2</option>
+                <option value="Type3">Type3</option>
+                <option value="Type4">Type4</option>
+                <option value="Type5">Type5</option>
+              </select>
+              <textarea name="description"></textarea>
               <button type="submit">+</button>
             </form>
           </div>
@@ -25,6 +32,7 @@ export default function taskManager() {
                 <div className="transaction-buttons">
                   <button className="edit-button">✏️</button>
                   <button className="delete-button">🗑️</button>
+                  <button className="task-complete">✔</button>
                 </div>
               </div>
               <p>Body Soap, Eggs, Bread, AAA batteries</p>
@@ -35,10 +43,22 @@ export default function taskManager() {
                 <div className="transaction-buttons">
                   <button className="edit-button">✏️</button>
                   <button className="delete-button">🗑️</button>
+                  <button className="task-complete">✔</button>
                 </div>
               </div>
               <p>Body Soap, Eggs, Bread, AAA batteries</p>
             </div>
+          </div>
+        </div>
+        <div className="right-section">
+          <div className="badges">
+            <img src="../public/images/first-launch.PNG" alt="first-launch"></img>
+            <img src="../public/images/first-orbit.PNG" alt="first-orbit"></img>
+            <img src="../public/images/galactic-traveler.PNG" alt="galactic-traveler"></img>
+            <img src="../public/images/rocket-rider.PNG" alt="rocket-rider"></img>
+            <img src="../public/images/star-seeker.PNG" alt="star-seeker"></img>
+            <img src="../public/images/stellar-navigator.PNG" alt="stellar-navigator"></img>
+            <p>test</p>
           </div>
         </div>
     </>
