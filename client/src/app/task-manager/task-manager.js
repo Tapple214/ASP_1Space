bf5cd068dac684d9d33fb619440f784ceb67096f
Thili02 <agilesoftwareprojectx@gmail.com>
2024-07-28 17:20:54 +0800

    Added input form using financial-organizer template

--- client/src/app/task-manager/task-manager.js
+++ client/src/app/task-manager/task-manager.js
@@ -1,11 +1,46 @@
-import "../home/Home.css";
+import "../task-manager/task-manager.css";
+import NavBar from "../../components/navbar/navbar";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import NavBar from "../../components/navbar/navbar";
 import { Navbar } from "react-bootstrap";
 
 export default function taskManager() {
   return(
-    <NavBar></NavBar>
+    <>
+      <NavBar />
+      <div className="right-section">
+          <div className="transaction-form">
+            <form>
+              <input type="text" placeholder="Title" />
+              <input type="date" placeholder="Current date" />
+              <input type="text" placeholder="Category" />
+              <input type="number" placeholder="$" />
+              <button type="submit">+</button>
+            </form>
+          </div>
+          <div className="transaction-list">
+            <div className="transaction-item">
+              <div className="transaction-header">
+                <h4>June 5 Hang out (Movies)</h4>
+                <div className="transaction-buttons">
+                  <button className="edit-button">✏️</button>
+                  <button className="delete-button">🗑️</button>
+                </div>
+              </div>
+              <p>Body Soap, Eggs, Bread, AAA batteries</p>
+            </div>
+            <div className="transaction-item">
+              <div className="transaction-header">
+                <h4>June 6 Groceries</h4>
+                <div className="transaction-buttons">
+                  <button className="edit-button">✏️</button>
+                  <button className="delete-button">🗑️</button>
+                </div>
+              </div>
+              <p>Body Soap, Eggs, Bread, AAA batteries</p>
+            </div>
+          </div>
+        </div>
+    </>
   )
 }
