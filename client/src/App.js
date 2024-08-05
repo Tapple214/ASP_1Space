4630be89992ee729bb970b73f249ce9b4f61c966
Tey Zi Hui <teyzihui9@gmail.com>
2024-08-05 15:01:47 +0800

    added link to budgethub from navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./app/home/Home";
 import Login from "./app/login/Login";
 import FinancialOrganizer from './app/financial-organizer/FinancialOrganizer';
+import BudgetHub from "./app/budgethub/budgethub";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/financial-organizer" element={<FinancialOrganizer />} />
+      <Route path="/budgethub" element={<BudgetHub />} />
       {/* Add other routes here */}
     </Routes>
   );
